3a47e9f2da57f58359f9799a63283fac76b3a72f
Alex Balli <alexanderjballi@gmail.com>
2024-03-23 11:46:35 -0700

    update: script.js

--- src/script.js
+++ src/script.js
@@ -23,13 +23,20 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 1.5)
+const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 scene.add(ambientLight)
 
-const pointLight = new THREE.PointLight(0xffffff, 50)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
+gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
+
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.9)
+directionalLight.position.set(1, 0.25, 0)
+scene.add(directionalLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.9)
+scene.add(hemisphereLight)
+
+const pointLight = new THREE.PointLight(0xff9000, 1.5)
+pointLight.position.set(1, - 0.5, 1)
 scene.add(pointLight)
 
 /**
